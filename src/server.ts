5e788b6a4f90eae98396d6092b7c01fa53c08ca2
haimhubara <haimhobara@gmail.com>
2025-08-28 20:21:07 +0300

    Add ip validation and route registrations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
 import firewallIPSRouter from "./routes/firewall_ips";
+import firewallURLRouter from "./routes/firewall_url";
+import firewallPortsRouter from "./routes/firewall_ports"; 
+import firewallRulesRouter from "./routes/firewall_rules"; 
 
 const app = express();
 app.use(express.json());
 app.use("/api/firewall", firewallIPSRouter);
+app.use("/api/firewall", firewallURLRouter);
+app.use("/api/firewall", firewallPortsRouter);
+app.use("/api/firewall", firewallRulesRouter);
 
 // app.get('/', (req: Request, res: Response) => {
 //   res.send('Hello, World!');
