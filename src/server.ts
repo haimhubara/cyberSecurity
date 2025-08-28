3eb023df255fb48f06494f53388ce087a93f057a
haimhubara <haimhobara@gmail.com>
2025-08-28 20:48:01 +0300

    Refactoring the Project Structure

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
-import express, { Request, Response } from 'express';
-import firewallIPSRouter from "./routes/firewall_ips";
-import firewallURLRouter from "./routes/firewall_url";
-import firewallPortsRouter from "./routes/firewall_ports"; 
-import firewallRulesRouter from "./routes/firewall_rules"; 
+import app from "./app";
 
-const app = express();
-app.use(express.json());
-app.use("/api/firewall", firewallIPSRouter);
-app.use("/api/firewall", firewallURLRouter);
-app.use("/api/firewall", firewallPortsRouter);
-app.use("/api/firewall", firewallRulesRouter);
+const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello, World!');
-// });
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
