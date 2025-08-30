e481d02b2b5763dbfb2976d5e9710b3e22d743a5
haimhubara <haimhobara@gmail.com>
2025-08-30 18:02:47 +0300

    Add logs

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import app from "./app";
 import config from "./config/env"; 
 import pool from "./config/db";
+import logger from "./config/logger";
 
 const PORT = config.port;
 
 app.listen(PORT, async () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  logger.info(`Server running on http://localhost:${PORT}`);
 
   try {
     const result = await pool.query("SELECT current_database();");
-    console.log("Connected to database:", result.rows[0].current_database);
+    logger.info(`Connected to database: ${result.rows[0].current_database}`);
   } catch (err) {
-    console.error("Error checking database:", err);
+    if (err instanceof Error) {
+      logger.error(`Error checking database: ${err.message}\n${err.stack}`);
+    } else {
+      logger.error(`Error checking database: ${err}`);
+    }
   }
 });
