924b6b85f9c181b724227e2b19fe8c6e2444115d
haimhubara <haimhobara@gmail.com>
2025-08-31 10:45:09 +0300

    Add Exponential Backoff

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,14 +6,22 @@ export const pool = new Pool({
   connectionString: config.databaseUri,
 });
 
+let attempt = 0;
+
 
 async function connectWithRetry() {
   try {
     await pool.query("SELECT 1");
     logger.info("Connected to database successfully!");
+     attempt = 0; // reset counter
   } catch (err) {
+    attempt++;
+    const delay = Math.min(
+      config.dbConnectionInterval * 2 ** (attempt - 1),
+      30000 // cap at 30 seconds
+    );
     logger.error(`DB connection failed: ${err}`);
-   logger.info(`Retrying in ${config.dbConnectionInterval}ms...`);
+    logger.info(`Retrying in ${config.dbConnectionInterval}ms...`);
 
     setTimeout(connectWithRetry,  config.dbConnectionInterval);
   }
